c81b7f0821450cc611dfac7784c65b01a9f5c266
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-03-15 14:23:13 -0400

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,14 @@
+class Book
+  attr_accessor :title
+  attr_accessor :author
+  attr_accessor :genre
+  attr_accessor :page_count
+  @@page_count = 0
+  def initialize(title)
+    @title = title
+  end
 
-
+  def turn_page
+    puts 'Flipping the page...wow, you read fast!'
+  end
+end
